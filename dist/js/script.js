9cb829c2cf8174bfe5deec0a637a14801e35e192
IgorShumsky <Tracho2013@gmail.com>
2025-02-21 09:32:25 +0200

    add new section "send us message"

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,30 +15,34 @@ function Burger_btn() {
 } Burger_btn();
 
 function headerAnimation() {
-  let h1_animation_text = document.querySelector('.h1_animation_text');
-  let h1 = document.querySelector('.header h1');
+  let _container_dancer_word = document.querySelectorAll('._container_dancer_word');
+  let _this_header = document.querySelectorAll('._this_header');
   let anim_style = ['ani1', 'ani2', 'ani3', 'ani4', 'ani5', 'ani6', 'ani7'];
-  h1.textContent.split(' ').forEach((letter, i) => {
+  _this_header.forEach((this_header, index) => {
 
-    function split_letter(elem) {
-      let item = '';
-      elem.split("").forEach((e) => {
-        const randomIndex = Math.floor(Math.random() * anim_style.length);
-        const RandomDelay = Math.round(Math.random() * 1.5 * 100) / 100;
-        let not_s = (e == " " ? true : false);
-        // item += `<span class="h1AminText" data-not_s="${not_s}">${e}</span>`;
-        item += `<span class="h1AminText" style="animation-delay:${RandomDelay}s;" data-text_anim="${anim_style[randomIndex]}">${e}</span>`;
+    this_header.textContent.split(' ').forEach((letter, i) => {
 
-      });
-      return (item);
-    }
-    if (letter !== "") {
-      h1_animation_text.insertAdjacentHTML("beforeend", `<div>${split_letter(letter)}</div>`);
-    }
+      function split_letter(elem) {
+        let item = '';
+        elem.split("").forEach((e) => {
+          const randomIndex = Math.floor(Math.random() * anim_style.length);
+          const RandomDelay = Math.round(Math.random() * 1.5 * 100) / 100;
+          let not_s = (e == " " ? true : false);
+          // item += `<span class="style_anim_letter" data-not_s="${not_s}">${e}</span>`;
+          item += `<span class="style_anim_letter" style="animation-delay:${RandomDelay}s;" data-text_anim="${anim_style[randomIndex]}">${e}</span>`;
+
+        });
+        return (item);
+      }
+      if (letter !== "") {
+        _container_dancer_word[index].insertAdjacentHTML("beforeend", `<div>${split_letter(letter)}</div>`);
+      }
+    });
   });
 
 
-  // h1_animation_text.childNodes.forEach((span)=>{
+
+  // _container_dancer_word.childNodes.forEach((span)=>{
   //   span.childNodes.forEach((e)=>{
   //     e.addEventListener("mouseover",()=>{
   //       console.log(e)
@@ -170,21 +174,21 @@ document.addEventListener("scroll", function () {
     img.style.transform = `translateY(${translateY}px)`;
   });
 });
- 
 
 
 
-let testSVG = document.getElementById("testSVG");
-let child = testSVG.children;
-console.log(testSVG)
-for (let i = 0; i < child.length; i++) {
-  let elem = child[i];
-  if (elem.tagName === "path") {
 
-    console.dir(i + "---" + Math.round(elem.getTotalLength()));
-    console.log(elem);
-  }
-}
+// let testSVG = document.getElementById("testSVG");
+// let child = testSVG.children;
+// console.log(testSVG)
+// for (let i = 0; i < child.length; i++) {
+//   let elem = child[i];
+//   if (elem.tagName === "path") {
+
+//     console.dir(i + "---" + Math.round(elem.getTotalLength()));
+//     console.log(elem);
+//   }
+// }
 
 
 
